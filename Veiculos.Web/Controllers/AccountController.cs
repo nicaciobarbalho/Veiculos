eae31aee30c955118e4157f65039ed8640dde2b9
Nicácio Barbalho <nicacio.costa@esig.com.br>
2018-09-03 17:22:12 -0300

    Desenvolvimento da login

--- Veiculos.Web/Controllers/AccountController.cs
+++ Veiculos.Web/Controllers/AccountController.cs
@@ -485,17 +485,25 @@ namespace Veiculos.Web.Controllers
         }
         private void IdentitySignIn(int userId, string userLogin)
         {
-            var claims = new List<Claim>();
-            claims.Add(new Claim(ClaimTypes.PrimarySid, userId.ToString()));
-            claims.Add(new Claim(ClaimTypes.Name, userLogin));
+            //var claims = new List<Claim>();
+            //claims.Add(new Claim(ClaimTypes.PrimarySid, userId.ToString()));
+            //claims.Add(new Claim(ClaimTypes.Name, userLogin));
+
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,userLogin),
+
+                    new Claim(ClaimTypes.Role, "User", "Admin", "SuperAdmin"),
+                    new Claim(ClaimTypes.PrimarySid,userId.ToString()),
+                };
 
             var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
 
-            //authenticationManager.SignIn(new AuthenticationProperties()
-            //{
-            //    ExpiresUtc = DateTime.UtcNow.AddDays(200),
-            //    IsPersistent = true
-            //}, identity);
+            var ctx = Request.GetOwinContext();
+            var auth = ctx.Authentication;
+
+            auth.SignIn(new AuthenticationProperties { IsPersistent = false }, identity);
+          
         }
         internal class ChallengeResult : HttpUnauthorizedResult
         {
